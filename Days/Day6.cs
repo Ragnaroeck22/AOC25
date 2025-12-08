9ea72dc2621c6c91597816f74e8206ddc643d000
Ragnaroeck22 <79421567+Ragnaroeck22@users.noreply.github.com>
2025-12-08 11:04:23 +0100

    Solve Day 6

--- Days/Day6.cs
+++ Days/Day6.cs
@@ -9,8 +9,9 @@ public class Day6
     
     public void Solve()
     {
-        ParseInput();
-
+        //ParseInput();
+        ParseInputPartTwo();
+        
         long sum = 0;
         for (int i = 0; i < numbers.Count; i++)
         {
@@ -18,7 +19,6 @@ public class Day6
             long solution = numbers[i][0];
             for (int j = 1; j < numbers[i].Count; j++)
             {
-                //Console.Write($"{solution} {op} {numbers[i][j]} = ");
                 switch (op)
                 {
                     case '+':
@@ -28,13 +28,48 @@ public class Day6
                         solution *= numbers[i][j];
                         break;
                 }
-                //Console.Write(solution + "\n");
             }
             sum += solution;
         }
-        Console.WriteLine($"Part 1: {sum}");
+        Console.WriteLine($"Solution: {sum}");
     }
 
+    private void ParseInputPartTwo()
+    {
+        // I hate this
+        var input = FileUtils.GetInputText(6);
+        
+        var lines = input.Split("\r\n");
+        for (int startIndex = 0; startIndex + 1 < lines[0].Length;)
+        {
+            List<long> nums = new();
+            
+            // Fuck you, AoC. I defy everything you stand for.
+            var l0 = Regex.Matches(lines[0].Substring(startIndex), @"[\d]+");
+            var l1 = Regex.Matches(lines[1].Substring(startIndex), @"[\d]+");
+            var l2 = Regex.Matches(lines[2].Substring(startIndex), @"[\d]+");
+            var l3 = Regex.Matches(lines[3].Substring(startIndex), @"[\d]+");
+            int maxLength = Math.Max(l0[0].Length, l1[0].Length);
+            maxLength = Math.Max(maxLength, l2[0].Length);
+            maxLength = Math.Max(maxLength, l3[0].Length);
+            
+            for (int x = 0; x < maxLength; x++)
+            {
+                int idx = startIndex + x;
+                string parseString = $"{lines[0][idx]}{lines[1][idx]}{lines[2][idx]}{lines[3][idx]}";
+                nums.Add(long.Parse(parseString));
+            }
+            numbers.Add(nums);
+            startIndex += maxLength + 1;
+        }
+        
+        var matchesOperators = Regex.Matches(input, @"[+*]+");
+        for (int i = 0; i < matchesOperators.Count; i++)
+        {
+            operators.Add(matchesOperators[i].Value[0]);
+        }
+    }
+    
     private void ParseInput()
     {
         var input = FileUtils.GetInputText(6);
@@ -48,8 +83,6 @@ public class Day6
         {
             numbers.Add(new ());
         }
-        
-        //Console.WriteLine($"Width: {numbers.Count}");
         
         for (int i = 0; i < matchesDigits.Count; i++)
         {
